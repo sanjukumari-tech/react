0643164b1dcf2ece47fdf13fa8b3962a204bbb2b
sanjukumari-tech <foodlazzez@gmail.com>
2025-04-15 08:29:41 +0530

    day-2parcel

--- App.js
+++ App.js
@@ -2,13 +2,14 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(headingg);
 // console.log(headingg)
-
+import React  from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child"},
        [
-        React.createElement("h1",{id:"heading"},"i am h1 tag"),
-        React.createElement("h2",{id:"heading"},"i am h2 tag"),
-        React.createElement("h3",{id:"heading"},"i am h3 tag")
+        React.createElement("h1",{id:"heading2"},"i am h1 tag"),
+        React.createElement("h2",{id:"heag3"},"i am h2 tag"),
+        React.createElement("h3",{id:"heading4"},"i am h3 tag")
        ]
     ),
     React.createElement("div",{id:"child2"},
